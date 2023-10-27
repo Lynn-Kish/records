d32968783262ebdbfc86a8b36880e7aea2767aee
lunakish <lunakish6@gmail.com>
2023-10-27 15:01:56 +0300

    first commit

--- src/CyclistList.js
+++ src/CyclistList.js
@@ -1,35 +1,22 @@
 import React, { Component } from 'react';
 
 class CyclistList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cyclists: [],
-    };
-  }
-
-  handleDelete = (index) => {
-    // Handle deletion logic, e.g., remove the cyclist from the list
-  };
-
-  handleEdit = (index) => {
-    // Handle edit logic, e.g., open a modal for editing
-  };
-
   render() {
-    const cyclists = this.state.cyclists.map((cyclist, index) => (
+    const { cyclists, handleEdit, handleDelete } = this.props;
+
+    const cyclistItems = cyclists.map((cyclist, index) => (
       <div key={index}>
         <p>Name: {cyclist.name}</p>
         {/* Display other cyclist information */}
-        <button type="button" onClick={() => this.handleEdit(index)}>Edit</button>
-        <button type="button" onClick={() => this.handleDelete(index)}>Delete</button>
+        <button type="button" onClick={() => handleEdit(index)}>Edit</button>
+        <button type="button" onClick={() => handleDelete(index)}>Delete</button>
       </div>
     ));
 
     return (
       <div>
         <h2>Registered Boda-Boda Cyclists</h2>
-        {cyclists}
+        {cyclistItems}
       </div>
     );
   }
